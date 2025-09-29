4210c364d82c8c3811c92225fc46e549d2f66d8e
zuzmo00 <safirmohammad33@gmail.com>
2025-09-29 10:26:32 +0200

    Add entity models and update project dependencies
    Introduced entity classes for FennmaradoNapok, Kerelmek, KulonlegesNapok, Log, Szemelyek, and Ugyintezok to support PTO management features. Updated PTO-Manager.csproj to include AutoMapper, JWT authentication, and Entity Framework Core packages. Added .gitignore and updated solution file to include it.

---
+++ PTO-Manager/Entities/Ugyintezok.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PTO_Manager.Entities
+{
+    public class Ugyintezok
+    {
+        [Key]
+        public Guid Id { get; set; } = new Guid();
+        public int Torzsszam { get; set; }
+        public string Email { get; set; }
+    }
+}
