cbf03c45f8f119428c581a57ce7c95b4b2ad0469
zuzmo00 <safirmohammad33@gmail.com>
2025-10-07 10:39:28 +0200

    Add user authentication and initial database schema
    Implemented user login functionality with JWT token generation, added UserController and UserServices, and created ApiResponse for standardized API responses. Updated AppDbContext with new entities and relationships, and added initial EF Core migrations for database schema. Changed password field in LoginInputDto to 'Jelszo', updated connection string, and improved project settings for documentation.

---
+++ PTO-Manager/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PTO_Manager.DTOs;
+using PTO_Manager.Entities;
+using PTO_Manager.Services;
+
+namespace PTO_Manager.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserServices _userService;
+        public UserController(IUserServices userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginInputDto user)
+        {
+            ApiResponse response = new ApiResponse();
+            try
+            {
+                var token = await _userService.Login(user);
+                response.Data = token;
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.StatusCode = 400;
+                response.Message = ex.Message;
+                response.Success = false;
+                return BadRequest(response);
+            }
+        }
+    }
+}
