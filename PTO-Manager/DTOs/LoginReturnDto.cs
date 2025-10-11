8c427245827a3174a0e05080aba32e0412ce8313
zuzmo00 <safirmohammad33@gmail.com>
2025-10-11 18:29:10 +0200

    Add admin department and permission management
    Introduces AddDepartment and ChangePermissions endpoints to AdminController, with corresponding service methods and DTOs. Refactors admin privileges DTO naming and updates login logic to return admin privileges. Also updates database connection string usage and cleans up appsettings.json.

--- PTO-Manager/DTOs/LoginReturnDto.cs
+++ PTO-Manager/DTOs/LoginReturnDto.cs
@@ -5,5 +5,5 @@ namespace PTO_Manager.DTOs;
 public class LoginReturnDto
 {
     public string token { get; set; }
-    public List<AdminPrivileges> adminPrivileges { get; set; }
+    public List<AdminPrivilegesDto>? adminPrivileges { get; set; }
 }
